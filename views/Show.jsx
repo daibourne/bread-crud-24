0ad7bfe9aa9cd1d60405fa905e66420df3fe7117
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-02-12 20:01:54 -0800

    mongoose added

---
+++ views/Show.jsx
@@ -0,0 +1,28 @@
+const React = require('react');
+const Default = require('./layouts/Default');
+
+const Show = ({ bread, index }) => {
+    return (
+        <Default>
+            <h2>Show Page</h2>
+            <h3>{bread.name}</h3>
+            <p>
+                and it
+                {bread.hasGluten ? <span> does </span> : <span> does NOT </span>}
+                have gluten.
+            </p>
+            <img src={bread.image} alt={bread.name} />
+            <li>
+                <a href='/breads'>Go home.</a>
+            </li>
+            <a className='button button-primary' href={`/breads/${index}/edit`}>
+                Edit
+            </a>
+            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+                <input type='submit' value='DELETE' />
+            </form>
+        </Default>
+    );
+};
+
+module.exports = Show;
